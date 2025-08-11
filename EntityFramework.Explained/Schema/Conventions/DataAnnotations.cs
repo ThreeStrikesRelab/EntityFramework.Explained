f85996ddb1e3ddb20f5b41a828d9425101c1f101
Mark Meyers <kilfour@gmail.com>
2025-08-11 17:00:15 +0200

    clening house

--- EntityFramework.Explained/Schema/Conventions/DataAnnotations.cs
+++ EntityFramework.Explained/Schema/Conventions/DataAnnotations.cs
@@ -8,55 +8,39 @@ using QuickPulse.Explains.Text;
 namespace EntityFramework.Explained.Schema.Conventions;
 
 [DocFile]
+[DocFileHeader("Data Annotations: `[Range(...)]`")]
 public class DataAnnotations
 {
+    [DocExample]
     public class Thing
     {
         public int Id { get; set; }
-
-        [Range(0, 10)]
+        [Range(0, 10)] // <= We are checking this 
         public int SecondInt { get; set; }
     }
-
+    
+    private LinesReader GetReader(DbContext context) 
+    {
+        using var context = context;
+        var sql = context.Database.GenerateCreateScript();
+        return LinesReader.FromText(sql);
+    }
+    
     [Fact]
-    [DocHeader("Sql Server - data annotations")]
-    [DocContent("looking for differences when using data annotations")]
+    [DocHeader("Sql Server")]
+    [DocContent("`[Range(0,10)]` gets ignored")]
     public void SqlServer()
     {
-        using var context = new TestSqlServerContext<Thing>();
-        var sql = context.Database.GenerateCreateScript();
-        var reader = LinesReader.FromText(sql);
-        Assert.Equal("CREATE TABLE [Items] (", reader.NextLine());
-        Assert.Equal("    [Id] int NOT NULL IDENTITY,", reader.NextLine());
-        Assert.Equal("    [SecondInt] int NOT NULL,", reader.NextLine());
-        Assert.Equal("    CONSTRAINT [PK_Items] PRIMARY KEY ([Id])", reader.NextLine());
-        Assert.Equal(");", reader.NextLine());
-        Assert.Equal("GO", reader.NextLine());
-        Assert.Equal("", reader.NextLine());
-        Assert.Equal("", reader.NextLine());
-        Assert.Equal("", reader.NextLine());
-        Assert.True(reader.EndOfContent());
-
-        //conclusion: Range(0,10) gets ignored
+        var result = GetReader(new TestSqlServerContext<Thing>()).SkipToLineContaining("SecondInt");
+        Assert.Equal("    [SecondInt] int NOT NULL,", result);
     }
 
     [Fact]
-    [DocHeader("Sqlite - data annotations")]
-    [DocContent("looking for differences when using data annotations")]
+    [DocHeader("Sqlite")]
+    [DocContent("Same behaviour as Sql Server.")]
     public void Sqlite()
     {
-        using var context = new TestSqliteContext<Thing>();
-        var sql = context.Database.GenerateCreateScript();
-        var reader = LinesReader.FromText(sql);
-        Assert.Equal("CREATE TABLE \"Items\" (", reader.NextLine());
-        Assert.Equal("    \"Id\" INTEGER NOT NULL CONSTRAINT \"PK_Items\" PRIMARY KEY AUTOINCREMENT,", reader.NextLine());
-        Assert.Equal("    \"SecondInt\" INTEGER NOT NULL", reader.NextLine());
-        Assert.Equal(");", reader.NextLine());
-        Assert.Equal("", reader.NextLine());
-        Assert.Equal("", reader.NextLine());
-        Assert.Equal("", reader.NextLine());
-        Assert.True(reader.EndOfContent());
-
-        //conslusion: Range(0,10) gets ignored
+        var result = GetReader(new TestSqliteContext<Thing>()).SkipToLineContaining("SecondInt");
+        Assert.Equal("    \"SecondInt\" INTEGER NOT NULL", result);
     }
 }
